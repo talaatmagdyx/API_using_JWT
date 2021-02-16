875449b1a54906f9d2c561ac9239b4d3cbcf30f6
TalaatMagdy <talaatmagdy75@gmail.com>
2021-02-17 01:41:28 +0200

    add authenticate for user

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,13 @@
 class ApiController < ApplicationController
+  include Knock::Authenticable
+
+  before_action :authenticate_user
   before_action :set_default_format
+  skip_before_action :verify_authenticity_token
+
+
+
+
   private
 
   def set_default_format
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_132836) do
+ActiveRecord::Schema.define(version: 2021_02_16_144036) do
 
   create_table "locations", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2021_02_16_132836) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["location_id"], name: "index_recordings_on_location_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
